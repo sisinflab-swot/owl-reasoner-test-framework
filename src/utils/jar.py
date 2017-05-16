146856590cebc5969806bc2395e388e356d07a49
Ivano Bilenchi <ivanobilenchi@gmail.com>
2017-05-16 16:15:43 +0200

    Add support for Java VM options

--- src/utils/jar.py
+++ src/utils/jar.py
@@ -2,16 +2,26 @@ import exc
 import proc
 
 
-def call(jar, args=None, output_action=proc.OutputAction.RETURN):
+def call(jar, args=None, vm_opts=None, output_action=proc.OutputAction.RETURN):
     """Executes a jar and returns its exit code and output.
 
     :param str jar : Path to the jar.
-    :param list args : Jar arguments.
+    :param list[str] args : Jar arguments.
+    :param list[str] vm_opts : Java VM options.
     :param proc.OutputAction output_action : What to do with the output.
     :rtype : proc.CallResult
     :return Call result object.
     """
     exc.raise_if_not_found(jar, file_type='file')
 
-    java_args = [proc.find_executable('java'), '-jar', jar] + args
+    java_args = [proc.find_executable('java')]
+
+    if vm_opts:
+        java_args.extend(vm_opts)
+
+    java_args.extend(['-jar', jar])
+
+    if args:
+        java_args.extend(args)
+
     return proc.call(java_args, output_action=output_action)
