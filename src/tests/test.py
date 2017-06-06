e17e59fb862381af7340afb28375459a0a3256c0
Ivano Bilenchi <ivanobilenchi@gmail.com>
2017-06-06 11:26:35 +0200

    Ensure logs are not cleared when switching dataset

--- src/tests/test.py
+++ src/tests/test.py
@@ -53,34 +53,34 @@ class Test(object):
             datasets = [path.join(data_dir, d) for d in listdir(data_dir)]
             datasets = [d for d in datasets if path.isdir(d)]
 
-        for dataset in datasets:
-            func_dir = path.join(dataset, 'functional')
-            xml_dir = path.join(dataset, 'rdfxml')
+        with Logger(config.Paths.LOG) as logger, open(config.Paths.RESULTS, mode='wb') as csv_file:
 
-            exc.raise_if_not_found(func_dir, file_type='dir')
-            exc.raise_if_not_found(xml_dir, file_type='dir')
-
-            onto_names = [f for f in listdir(func_dir) if f.endswith('.owl')]
-            logger = Logger(config.Paths.LOG)
-
-            # Hello
-            echo.pretty(
-                'Starting {} test on "{}" dataset ({} ontologies)...'.format(self.name,
-                                                                             path.basename(dataset),
-                                                                             len(onto_names)),
-                color=echo.Color.GREEN)
-
-            # Setup/cleanup
             fileutils.create_dir(config.Paths.WRK_DIR)
             fileutils.create_dir(config.Paths.TEMP_DIR)
             fileutils.remove_dir_contents(config.Paths.TEMP_DIR)
+
             logger.clear()
+            csv_writer = csv.writer(csv_file)
 
-            # Start test
-            with logger, open(config.Paths.RESULTS, mode='wb') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                self.setup(logger, csv_writer)
+            self.setup(logger, csv_writer)
 
+            for dataset in datasets:
+                func_dir = path.join(dataset, 'functional')
+                xml_dir = path.join(dataset, 'rdfxml')
+
+                exc.raise_if_not_found(func_dir, file_type='dir')
+                exc.raise_if_not_found(xml_dir, file_type='dir')
+
+                onto_names = [f for f in listdir(func_dir) if f.endswith('.owl')]
+
+                # Hello
+                echo.pretty(
+                    'Starting {} test on "{}" dataset ({} ontologies)...'.format(self.name,
+                                                                                 path.basename(dataset),
+                                                                                 len(onto_names)),
+                    color=echo.Color.GREEN)
+
+                # Test dataset
                 for onto_name in onto_names:
                     func_ontology = OWLOntology(path.join(func_dir, onto_name), OWLSyntax.FUNCTIONAL)
                     xml_ontology = OWLOntology(path.join(xml_dir, onto_name), OWLSyntax.RDFXML)
